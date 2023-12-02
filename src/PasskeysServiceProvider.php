9a0e23e6a100789978ed5769c5800b19c3fc82e0
Michal Štván <michalstvan@gmail.com>
2023-12-02 22:47:16 +0100

    make service provider ready for publishing

--- src/PasskeysServiceProvider.php
+++ src/PasskeysServiceProvider.php
@@ -7,8 +7,13 @@ use Illuminate\Support\ServiceProvider;
 class PasskeysServiceProvider extends ServiceProvider {
 
     public function boot(): void {
+        $this->publishes([
+            __DIR__ . '/../config/passkeys.php' => config_path('passkeys.php'),
+        ], 'laravel-passkeys-config');
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ], 'laravel-passkeys-migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/passkeys.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     public function register(): void {
