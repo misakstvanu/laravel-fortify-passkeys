30da9e15af4e8b7329a9bb18895c5b0b81ac1670
Michal Štván <michalstvan@gmail.com>
2023-12-02 22:46:16 +0100

    add config options

--- config/passkeys.php
+++ config/passkeys.php
@@ -2,13 +2,21 @@
 
 return [
 
+    'user_model' => App\Models\User::class,
+
     'route_prefix' => 'passkey',
+
+    'route_middleware' => ['web'],
 
     /*
      * This is just a fancy name for "domains that should not require HTTPS".
      * This is useful for local development or complex networks.
      * THIS OPTION IS DANGEROUS, USE CAREFULLY!
      */
-    'relying_party_ids' => []
+    'relying_party_ids' => [],
+
+    'username_column' => 'email',
+
+    'registration_user_validation' => [],
 
 ];
