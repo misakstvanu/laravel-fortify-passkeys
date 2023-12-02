5fd33655acb5f9a7fabd870bebc8625075185610
Michal Štván <michalstvan@gmail.com>
2023-12-02 22:46:33 +0100

    apply configs to routes

--- routes/passkeys.php
+++ routes/passkeys.php
@@ -4,17 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => config('passkeys.route_prefix'),
-    'middleware' => ['web']
+    'middleware' => config('passkeys.route_middleware')
 ], function () {
+    Route::post('login/options', [\Misakstvanu\LaravelPasskeys\Controllers\AuthenticationController::class, 'generateOptions'])->name('passkeys.login.start');
+    Route::post('login', [\Misakstvanu\LaravelPasskeys\Controllers\AuthenticationController::class, 'verify'])->name('passkeys.login.verify');
 
-    //fetch authentication options based on
-    Route::post('login/options', [\Misakstvanu\LaravelPasskeys\Controllers\AuthenticationController::class, 'generateOptions']);
-
-    Route::post('login', [\Misakstvanu\LaravelPasskeys\Controllers\AuthenticationController::class, 'verify']);
-
-
-    Route::post('register/options', [\Misakstvanu\LaravelPasskeys\Controllers\RegistrationController::class, 'generateOptions']);
-
-    Route::post('register', [\Misakstvanu\LaravelPasskeys\Controllers\RegistrationController::class, 'verify']);
+    Route::post('register/options', [\Misakstvanu\LaravelPasskeys\Controllers\RegistrationController::class, 'generateOptions'])->name('passkeys.register.start');
+    Route::post('register', [\Misakstvanu\LaravelPasskeys\Controllers\RegistrationController::class, 'verify'])->name('passkeys.register.verify');
 });
 
