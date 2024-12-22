4ced16ac49c5445f16177049233f91bb45ff2a2a
Michal Štván <michalstvan@gmail.com>
2024-12-22 23:18:41 +0100

    fix config routes

--- routes/passkeys.php
+++ routes/passkeys.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Misakstvanu\LaravelFortifyPasskeys\Controllers\AuthenticationController;
+use Misakstvanu\LaravelFortifyPasskeys\Controllers\RegistrationController;
 
 Route::group([
-    'prefix' => config('fortify.prefix'),
+    'prefix' => config('passkeys.route_prefix'),
     'middleware' => config('passkeys.route_middleware')
 ], function () {
-    Route::post('passkey/login/options', [\Misakstvanu\LaravelFortifyPasskeys\Controllers\AuthenticationController::class, 'generateOptions'])->name('passkeys.login.start');
-    Route::post('passkey/login', [\Misakstvanu\LaravelFortifyPasskeys\Controllers\AuthenticationController::class, 'verify'])->name('passkeys.login.verify');
+    Route::post('passkey/login/options', [AuthenticationController::class, 'generateOptions'])->name('passkeys.login.start');
+    Route::post('passkey/login', [AuthenticationController::class, 'verify'])->name('passkeys.login.verify');
 
-    Route::post('passkey/register/options', [\Misakstvanu\LaravelFortifyPasskeys\Controllers\RegistrationController::class, 'generateOptions'])->name('passkeys.register.start');
-    Route::post('passkey/register', [\Misakstvanu\LaravelFortifyPasskeys\Controllers\RegistrationController::class, 'verify'])->name('passkeys.register.verify');
+    Route::post('passkey/register/options', [RegistrationController::class, 'generateOptions'])->name('passkeys.register.start');
+    Route::post('passkey/register', [RegistrationController::class, 'verify'])->name('passkeys.register.verify');
 });
 
